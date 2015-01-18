d6402a0e5d9fc2c3bc882551f498e90afd66f06a
Jason Angus <ja6a61@gmail.com>
2015-01-18 14:13:41 +0800

    Added vim *.swp files to .gitignore. Finished output problem.

--- ch02/op-pattern.cpp
+++ ch02/op-pattern.cpp
@@ -7,8 +7,10 @@ using std::cin;
 using std::cout;
 
 int main(void) {
-  for (int row = 1; row <= 5; row++) {
-    for (int hashNum = 1; hashNum <= 6 - row; hashNum++) {
+  for (int row = 1; row <= 7; row++) {
+    //for (int hashNum = 1; hashNum <= 6 - row; hashNum++) {
+    //for (int hashNum = 1; hashNum <= 4-abs(4-row); hashNum++) {
+    for (int hashNum = 1; hashNum <= 4-abs(4-row); hashNum++) {
       cout << "#";
     }
     cout << "\n";
